867199e6f1c00b7c9657ad95b122a89174d5973d
Soren Ptak <ptaksoren@gmail.com>
2023-08-03 05:33:25 -0700

    Swapping to clang-format

--- source/backoff_algorithm.c
+++ source/backoff_algorithm.c
@@ -4,28 +4,29 @@
  *
  * SPDX-License-Identifier: MIT
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 /**
  * @file backoff_algorithm.c
- * @brief Implementation of the backoff algorithm API for a "Full Jitter" exponential backoff
- * with jitter strategy.
+ * @brief Implementation of the backoff algorithm API for a "Full Jitter"
+ * exponential backoff with jitter strategy.
  */
 
 /* Standard includes. */
@@ -37,31 +38,38 @@
 
 /*-----------------------------------------------------------*/
 
-BackoffAlgorithmStatus_t BackoffAlgorithm_GetNextBackoff( BackoffAlgorithmContext_t * pRetryContext,
-                                                          uint32_t randomValue,
-                                                          uint16_t * pNextBackOff )
+BackoffAlgorithmStatus_t BackoffAlgorithm_GetNextBackoff(
+    BackoffAlgorithmContext_t * pRetryContext,
+    uint32_t randomValue,
+    uint16_t * pNextBackOff )
 {
     BackoffAlgorithmStatus_t status = BackoffAlgorithmSuccess;
 
     assert( pRetryContext != NULL );
     assert( pNextBackOff != NULL );
 
-    /* If maxRetryAttempts state of the context is set to the maximum, retry forever. */
-    if( ( pRetryContext->maxRetryAttempts == BACKOFF_ALGORITHM_RETRY_FOREVER ) ||
+    /* If maxRetryAttempts state of the context is set to the maximum, retry
+     * forever. */
+    if( ( pRetryContext->maxRetryAttempts ==
+          BACKOFF_ALGORITHM_RETRY_FOREVER ) ||
         ( pRetryContext->attemptsDone < pRetryContext->maxRetryAttempts ) )
     {
-        /* The next backoff value is a random value between 0 and the maximum jitter value
-         * for the retry attempt. */
+        /* The next backoff value is a random value between 0 and the maximum
+         * jitter value for the retry attempt. */
 
-        /* Choose a random value for back-off time between 0 and the max jitter value. */
-        *pNextBackOff = ( uint16_t ) ( randomValue % ( pRetryContext->nextJitterMax + ( uint32_t ) 1U ) );
+        /* Choose a random value for back-off time between 0 and the max jitter
+         * value. */
+        *pNextBackOff = ( uint16_t ) ( randomValue %
+                                       ( pRetryContext->nextJitterMax +
+                                         ( uint32_t ) 1U ) );
 
         /* Increment the retry attempt. */
         pRetryContext->attemptsDone++;
 
         /* Double the max jitter value for the next retry attempt, only
          * if the new value will be less than the max backoff time value. */
-        if( pRetryContext->nextJitterMax < ( pRetryContext->maxBackoffDelay / 2U ) )
+        if( pRetryContext->nextJitterMax <
+            ( pRetryContext->maxBackoffDelay / 2U ) )
         {
             pRetryContext->nextJitterMax += pRetryContext->nextJitterMax;
         }
@@ -74,7 +82,8 @@ BackoffAlgorithmStatus_t BackoffAlgorithm_GetNextBackoff( BackoffAlgorithmContex
     {
         /* When max retry attempts are exhausted, let application know by
          * returning BackoffAlgorithmRetriesExhausted. Application may choose to
-         * restart the retry process after calling BackoffAlgorithm_InitializeParams(). */
+         * restart the retry process after calling
+         * BackoffAlgorithm_InitializeParams(). */
         status = BackoffAlgorithmRetriesExhausted;
     }
 
--- source/include/backoff_algorithm.h
+++ source/include/backoff_algorithm.h
@@ -4,22 +4,23 @@
  *
  * SPDX-License-Identifier: MIT
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 /**
@@ -40,7 +41,7 @@
 
 /* *INDENT-OFF* */
 #ifdef __cplusplus
-    extern "C" {
+extern "C" {
 #endif
 /* *INDENT-ON* */
 
@@ -48,7 +49,7 @@
  * @ingroup backoff_algorithm_constants
  * @brief Constant to represent unlimited number of retry attempts.
  */
-#define BACKOFF_ALGORITHM_RETRY_FOREVER    ( UINT32_MAX )
+#define BACKOFF_ALGORITHM_RETRY_FOREVER ( UINT32_MAX )
 
 /**
  * @ingroup backoff_algorithm_enum_types
@@ -56,30 +57,35 @@
  */
 typedef enum BackoffAlgorithmStatus
 {
-    BackoffAlgorithmSuccess = 0,     /**< @brief The function successfully calculated the next back-off value. */
-    BackoffAlgorithmRetriesExhausted /**< @brief The function exhausted all retry attempts. */
+    BackoffAlgorithmSuccess = 0,     /**< @brief The function successfully
+                                        calculated the next back-off value. */
+    BackoffAlgorithmRetriesExhausted /**< @brief The function exhausted all
+                                        retry attempts. */
 } BackoffAlgorithmStatus_t;
 
 /**
  * @ingroup backoff_algorithm_struct_types
- * @brief Represents parameters required for calculating the back-off delay for the
- * next retry attempt.
+ * @brief Represents parameters required for calculating the back-off delay for
+ * the next retry attempt.
  */
 typedef struct BackoffAlgorithmContext
 {
     /**
-     * @brief The maximum backoff delay (in milliseconds) between consecutive retry attempts.
+     * @brief The maximum backoff delay (in milliseconds) between consecutive
+     * retry attempts.
      */
     uint16_t maxBackoffDelay;
 
     /**
      * @brief The total number of retry attempts completed.
-     * This value is incremented on every call to #BackoffAlgorithm_GetNextBackoff API.
+     * This value is incremented on every call to
+     * #BackoffAlgorithm_GetNextBackoff API.
      */
     uint32_t attemptsDone;
 
     /**
-     * @brief The maximum backoff value (in milliseconds) for the next retry attempt.
+     * @brief The maximum backoff value (in milliseconds) for the next retry
+     * attempt.
      */
     uint16_t nextJitterMax;
 
@@ -92,7 +98,8 @@ typedef struct BackoffAlgorithmContext
 /**
  * @brief Initializes the context for using backoff algorithm. The parameters
  * are required for calculating the next retry backoff delay.
- * This function must be called by the application before the first new retry attempt.
+ * This function must be called by the application before the first new retry
+ * attempt.
  *
  * @param[out] pContext The context to initialize with parameters required
  * for the next backoff delay calculation function.
@@ -114,33 +121,36 @@ void BackoffAlgorithm_InitializeParams( BackoffAlgorithmContext_t * pContext,
  * @brief Simple exponential backoff and jitter function that provides the
  * delay value for the next retry attempt.
  * After a failure of an operation that needs to be retried, the application
- * should use this function to obtain the backoff delay value for the next retry,
- * and then wait for the backoff time period before retrying the operation.
+ * should use this function to obtain the backoff delay value for the next
+ * retry, and then wait for the backoff time period before retrying the
+ * operation.
  *
- * @param[in, out] pRetryContext Structure containing parameters for the next backoff
- * value calculation.
- * @param[in] randomValue The random value to use for calculation of the backoff period.
- * The random value should be in the range of [0, UINT32_MAX].
- * @param[out] pNextBackOff This will be populated with the backoff value (in milliseconds)
- * for the next retry attempt. The value does not exceed the maximum backoff delay
- * configured in the context.
+ * @param[in, out] pRetryContext Structure containing parameters for the next
+ * backoff value calculation.
+ * @param[in] randomValue The random value to use for calculation of the backoff
+ * period. The random value should be in the range of [0, UINT32_MAX].
+ * @param[out] pNextBackOff This will be populated with the backoff value (in
+ * milliseconds) for the next retry attempt. The value does not exceed the
+ * maximum backoff delay configured in the context.
  *
- * @note For generating a random number, it is recommended to use a Random Number Generator
- * that is seeded with a device-specific entropy source so that possibility of collisions
- * between multiple devices retrying the network operations can be mitigated.
+ * @note For generating a random number, it is recommended to use a Random
+ * Number Generator that is seeded with a device-specific entropy source so that
+ * possibility of collisions between multiple devices retrying the network
+ * operations can be mitigated.
  *
  * @return #BackoffAlgorithmSuccess after a successful sleep;
  * #BackoffAlgorithmRetriesExhausted when all attempts are exhausted.
  */
 /* @[define_backoffalgorithm_getnextbackoff] */
-BackoffAlgorithmStatus_t BackoffAlgorithm_GetNextBackoff( BackoffAlgorithmContext_t * pRetryContext,
-                                                          uint32_t randomValue,
-                                                          uint16_t * pNextBackOff );
+BackoffAlgorithmStatus_t BackoffAlgorithm_GetNextBackoff(
+    BackoffAlgorithmContext_t * pRetryContext,
+    uint32_t randomValue,
+    uint16_t * pNextBackOff );
 /* @[define_backoffalgorithm_getnextbackoff] */
 
 /* *INDENT-OFF* */
 #ifdef __cplusplus
-    }
+}
 #endif
 /* *INDENT-ON* */
 
